0cc4fca39e112f82eefcb4213345b66a22ef11f2
Wintermute6 <hannes.up@gmail.com>
2015-09-14 02:27:04 +0200

    trying to get postitem into postboardActivity

--- app/src/main/java/de/ur/mi/kilroy/kilroyapp/PostboardActivity.java
+++ app/src/main/java/de/ur/mi/kilroy/kilroyapp/PostboardActivity.java
@@ -3,7 +3,10 @@ package de.ur.mi.kilroy.kilroyapp;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import de.ur.mi.kilroy.kilroyapp.adapters.CommentItemAdapter;
 
 /**
  * Created by simon on 13/09/15.
@@ -17,8 +20,11 @@ public class PostboardActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postboard);
         initUi();
+        setupListview();
 
     }
+
+
 
     private void initUi() {
         nameView = (TextView) findViewById(R.id.titleView);
@@ -27,5 +33,11 @@ public class PostboardActivity extends ListActivity {
         Intent i = getIntent();
         nameView.setText(i.getStringExtra("name"));
         descriptionView.setText(i.getStringExtra("description"));
+    }
+
+    private void setupListview() {
+        ListView listView =  getListView();
+
+        listView.setAdapter(new CommentItemAdapter(this,null, ));
     }
 }
--- app/src/main/java/de/ur/mi/kilroy/kilroyapp/adapters/CommentItemAdapter.java
+++ app/src/main/java/de/ur/mi/kilroy/kilroyapp/adapters/CommentItemAdapter.java
@@ -6,9 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
+import de.ur.mi.kilroy.kilroyapp.R;
 import de.ur.mi.kilroy.kilroyapp.items.CommentItem;
 
 /**
@@ -21,6 +25,7 @@ public class CommentItemAdapter extends ArrayAdapter<CommentItem> {
     private Context context;
     private List objects;
 
+
     public CommentItemAdapter(Context context, int resource) {
         super(context, resource);
     }
@@ -32,16 +37,31 @@ public class CommentItemAdapter extends ArrayAdapter<CommentItem> {
         inflater = LayoutInflater.from(context);
         this.context = context;
         this.objects = objects;
+
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         //return super.getView(position, convertView, parent);
-        View row = convertView;
+        View v;
 
-        if(row == null)
-        {
-            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            row = inflater.inflate(resource, parent, false);
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.item_comment, null);
+        } else {
+            v = convertView;
+        }
+
+        TextView author = (TextView) v.findViewById(R.id.author_textView);
+        TextView date = (TextView) v.findViewById(R.id.date_textView);
+        TextView content = (TextView) v.findViewById(R.id.content_textView);
+
+        CommentItem item = (CommentItem) this.objects.get(position);
+
+        author.setText(item.getAuthor());
+        date.setText(item.getSubmission_date().toString());
+        content.setText(item.getContent());
+
+        return v;
     }
 }
