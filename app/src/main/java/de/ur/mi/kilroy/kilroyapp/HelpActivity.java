ed98a2849b7c331fbbc318715c382c3765db1bcd
Wintermute6 <hannes.up@gmail.com>
2015-09-24 17:25:22 +0200

    hilfe activity und dokumentation

---
+++ app/src/main/java/de/ur/mi/kilroy/kilroyapp/HelpActivity.java
@@ -0,0 +1,16 @@
+package de.ur.mi.kilroy.kilroyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Wintermute on 24.09.2015.
+ */
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
